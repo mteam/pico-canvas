4d5a5f99b8211483a03e3cbea686f065e2576431
Michal Miškerník <michal@miskernik.sk>
2023-09-19 22:51:02 +0200

    Rect fill

--- src/include/picocanvas.hpp
+++ src/include/picocanvas.hpp
@@ -9,6 +9,12 @@ namespace picocanvas {
     public:
         Canvas(uint16_t width, uint16_t height);
 
+        void fill(uint16_t color) {
+            fill_rect(bounds, color);
+        }
+
+        void fill_rect(const Rect &rect, uint16_t color);
+
     protected:
         uint16_t *frame_buffer;
         Rect bounds;
--- src/picocanvas.cpp
+++ src/picocanvas.cpp
@@ -4,4 +4,12 @@ namespace picocanvas {
     Canvas::Canvas(uint16_t width, uint16_t height) : bounds(0, 0, width, height) {
         frame_buffer = new uint16_t[width * height];
     }
+
+    void Canvas::fill_rect(const Rect &rect, uint16_t color) {
+        for (int y = rect.y; y < (rect.y + rect.h); y++) {
+            for (int x = rect.x; x < (rect.x + rect.w); x++) {
+                frame_buffer[y * bounds.w + x] = color;
+            }
+        }
+    }
 }
