5eb9adcc1d31e77a16669d7bc51e9000974b4dac
Michal Miškerník <michal@miskernik.sk>
2023-09-19 23:23:53 +0200

    Accessor for frame buffer array

--- src/include/picocanvas.hpp
+++ src/include/picocanvas.hpp
@@ -2,12 +2,15 @@
 
 #include <cstdint>
 #include <array>
+#include <cstddef>
 #include "picocanvas/geometry.hpp"
 
 namespace picocanvas {
     class Canvas {
     public:
         Canvas(uint16_t width, uint16_t height);
+
+        uint16_t *get_frame_buffer() { return frame_buffer; }
 
         void fill(uint16_t color) {
             fill_rect(bounds, color);
