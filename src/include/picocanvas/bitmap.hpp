1b017c51da1a48d1fbfed3526478f01dd0388296
Michal Miškerník <michal@miskernik.sk>
2023-10-30 21:26:54 +0100

    Optimized BitmapBMP struct memory usage

--- src/include/picocanvas/bitmap.hpp
+++ src/include/picocanvas/bitmap.hpp
@@ -70,34 +70,22 @@ namespace picocanvas {
 
     struct BitmapBMP {
         const uint8_t *data;
-        const BMPHeader *bmp_header;
-        const DIBHeader *dib_header;
-        const BMPColor *palette;
-        const uint8_t *pixels;
-        const std::size_t length;
+        uint32_t cursor = 0;
 
-        unsigned int cursor = 0;
-
-        BitmapBMP(const uint8_t *data, std::size_t length) :
-                data(data),
-                bmp_header(reinterpret_cast<const BMPHeader *>(data)),
-                dib_header(reinterpret_cast<const DIBHeader *>(data + sizeof(BMPHeader))),
-                palette(reinterpret_cast<const BMPColor *>(data + sizeof(BMPHeader) + dib_header->header_size)),
-                pixels(data + bmp_header->data_offset),
-                length(length) {}
+        BitmapBMP(const uint8_t *data, std::size_t length) : data(data) {}
 
         explicit BitmapBMP(std::vector<uint8_t> data) : BitmapBMP(data.data(), data.size()) {}
 
         Rect rect() const {
-            return Rect{0, 0, static_cast<int32_t>(dib_header->width), static_cast<int32_t>(dib_header->height)};
+            return Rect{0, 0, static_cast<int32_t>(dib_header()->width), static_cast<int32_t>(dib_header()->height)};
         }
 
         std::vector<BMPColor> palette_vector() const {
             std::vector<BMPColor> palette_vector;
-            palette_vector.reserve(dib_header->colors_used);
+            palette_vector.reserve(dib_header()->colors_used);
 
-            for (int i = 0; i < dib_header->colors_used; i++) {
-                palette_vector.push_back(palette[i]);
+            for (int i = 0; i < dib_header()->colors_used; i++) {
+                palette_vector.push_back(palette()[i]);
             }
 
             return palette_vector;
@@ -105,16 +93,16 @@ namespace picocanvas {
 
         void set_cursor(uint16_t x, uint16_t y) {
             // BMPs are stored upside down
-            y = dib_header->height - y - 1;
+            y = dib_header()->height - y - 1;
 
-            uint8_t bytes_per_pixel = dib_header->bits_per_pixel / 8;
-            uint32_t row_size = (dib_header->width * bytes_per_pixel + 3) & -4;
+            uint8_t bytes_per_pixel = dib_header()->bits_per_pixel / 8;
+            uint32_t row_size = (dib_header()->width * bytes_per_pixel + 3) & -4;
             cursor = y * row_size + x;
         }
 
         uint8_t sample_color() const {
-            if (dib_header->bits_per_pixel == 8) {
-                return pixels[cursor];
+            if (dib_header()->bits_per_pixel == 8) {
+                return pixels()[cursor];
             } else {
                 return 0;
             }
@@ -123,6 +111,22 @@ namespace picocanvas {
         uint8_t sample_color(uint16_t x, uint16_t y) {
             set_cursor(x, y);
             return sample_color();
+        }
+    private:
+        inline const BMPHeader *bmp_header() const {
+            return reinterpret_cast<const BMPHeader *>(data);
+        }
+
+        inline const DIBHeader *dib_header() const {
+            return reinterpret_cast<const DIBHeader *>(data + sizeof(BMPHeader));
+        }
+
+        inline const BMPColor *palette() const {
+            return reinterpret_cast<const BMPColor *>(data + sizeof(BMPHeader) + dib_header()->header_size);
+        }
+
+        inline const uint8_t *pixels() const {
+            return data + bmp_header()->data_offset;
         }
     };
 }
